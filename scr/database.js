74c61b6499549e952f7366867e0ac9aed83bb7df
Karol López M <83683023+klopezm@users.noreply.github.com>
2021-09-05 22:23:39 -0500

    Create database.js

---
+++ scr/database.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+mongoose.connect("mongodb://Localhost/companydb",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    
+   
+})
+
+
+.then(db => console.log('Db is connected'))
+.catch(error => console.log(error))
