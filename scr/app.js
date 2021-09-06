f9e9413201d24d7ae1dc9cb350d28a22ce501fa1
Karol López M <83683023+klopezm@users.noreply.github.com>
2021-09-05 22:21:50 -0500

    Update app.js

--- scr/app.js
+++ scr/app.js
@@ -1,1 +1,27 @@
+import express from 'express'
+import morgan from 'morgan'
 
+import {createRole, createRoles} from './libs/initialSetup'
+const app = express()
+createRoles();
+
+import productsRoutes from './routes/products.routes'
+import authRoutes from './routes/auth.routes'
+import usersRoutes from './routes/user.routes'
+app.use(express.json())
+app.use(morgan('dev'));
+
+
+app.get('/', (req, res) => {
+    res.json({
+        author: 'elena',
+        description: "",
+        version: "1.0.0"
+    })
+})
+
+app.use('/api/products', productsRoutes)
+app.use('/api/auth', authRoutes);
+app.use('/api/users', usersRoutes)
+
+export default app;
